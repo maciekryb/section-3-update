86415df8ece49f4dc925e3edbf456b10df4d1799
Maciej Rybak <maciek.rybak95@gmail.com>
2022-11-13 11:51:30 +0100

    Shopping list- with useState

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
 import './App.css';
 import React from 'react';
 
-class App extends React.Component {
-  state = {
+const App = () => {
+  const [items] = React.useState({
     items1: 'ogorki',
     items2: 'sok',
     items3: 'cos do picia',
-  };
+  });
 
-  render() {
-    return (
-      <>
-        <h1>Lista zakupow</h1>
-        <ul>
-          <ItemList name={this.state.items1} examplge={2 + 2} />
-          <ItemList name={this.state.items2} />
-          <ItemList name={this.state.items3} />
-        </ul>
-      </>
-    );
-  }
-}
+  return (
+    <>
+      <h1>Lista zakupow</h1>
+      <ul>
+        <ItemList name={items.items1} examplge={2 + 2} />
+        <ItemList name={items.items2} />
+        <ItemList name={items.items3} />
+      </ul>
+    </>
+  );
+};
 
-class ItemList extends React.Component {
-  render() {
-    return (
-      <li>
-        {this.props.name} - {this.props.examplge}
-      </li>
-    );
-  }
-}
+const ItemList = ({ name, examplge }) => (
+  <li>
+    {name} - {examplge}
+  </li>
+);
 
 export default App;
