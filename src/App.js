8d48a6e613fb3885e1477a57a7a57f7ff5055b4a
Maciej Rybak <maciek.rybak95@gmail.com>
2022-11-13 15:27:06 +0100

    Simple useState

--- src/App.js
+++ src/App.js
@@ -1,58 +1,17 @@
+import React, { useState } from "react";
 import "./App.css";
-import React from "react";
-
-const ADDITION = "addition";
-const RESET = "reset";
-const SUBTRACTION = "subtraction";
 
 const App = (props) => {
-  const [count, setCount] = React.useState(0);
-  const [result, setResult] = React.useState(props.result);
-
-  const handleMathClick = (type, number = 1) => {
-    if (type === SUBTRACTION) {
-      setCount(count + 1);
-      setResult(result - number);
-    } else if (type === "reset") {
-      setCount(count + 1);
-      setResult(0);
-    } else {
-      setCount(count + 1);
-      setResult(result + number);
-    }
-  };
+  const [state, setState] = useState({ a: 0, b: 10 });
+  // const [info, setInfo] = useState("nie łączymy w jeden state");
+  const handler = () => setState({ ...state, a: state.a + 1 });
 
   return (
-    <>
-      <MathButton name="-10" number={10} type={SUBTRACTION} click={handleMathClick} />
-      <MathButton name="-1" number={1} type={SUBTRACTION} click={handleMathClick} />
-      <MathButton name="Reset" type={RESET} click={handleMathClick} />
-      <MathButton name="1" number={1} type={ADDITION} click={handleMathClick} />
-      <MathButton name="10" number={10} type={ADDITION} click={handleMathClick} />
-      <ResultPanel count={count} result={result} />
-    </>
-  );
-};
-
-const MathButton = ({ click, name, number, type }) => {
-  const handleOnClick = () => click(type, number);
-  return (
-    <>
-      <button onClick={handleOnClick}>{name}</button>
-    </>
-  );
-};
-
-const ResultPanel = ({ count, result }) => {
-  const addionalInformation = count > 10 ? <span>Przeciązenie procesora</span> : null;
-  return (
-    <>
-      <h1>
-        Liczba klknięć: {count}
-        {addionalInformation}
-      </h1>
-      <h2>Wynik: {result}</h2>
-    </>
+    <div onClick={handler}>
+      <h1>{state.a}</h1>
+      <h2>{state.b}</h2>
+      <p>Lorem ipsum</p>
+    </div>
   );
 };
 
