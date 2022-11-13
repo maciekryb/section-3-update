eefb2a1ce2da33ce0e979d0ac0c6bedf38687475
Maciej Rybak <maciek.rybak95@gmail.com>
2022-11-13 11:53:00 +0100

    Default App

--- src/App.js
+++ src/App.js
@@ -1,29 +1,27 @@
 import './App.css';
 import React from 'react';
 
-const App = () => {
-  const [items] = React.useState({
-    items1: 'ogorki',
-    items2: 'sok',
-    items3: 'cos do picia',
-  });
+class Addsign extends React.Component {
+  state = {
+    text: 'a',
+  };
 
-  return (
-    <>
-      <h1>Lista zakupow</h1>
-      <ul>
-        <ItemList name={items.items1} examplge={2 + 2} />
-        <ItemList name={items.items2} />
-        <ItemList name={items.items3} />
-      </ul>
-    </>
-  );
-};
+  handleAdd = () => {
+    const letter = 'a';
+    this.setState({
+      text: this.state.text + letter,
+    });
+  };
 
-const ItemList = ({ name, examplge }) => (
-  <li>
-    {name} - {examplge}
-  </li>
-);
+  render() {
+    return (
+      <>
+        <button onClick={this.handleAdd}>Dodaj a</button>
+        <br />
+        {this.state.text}
+      </>
+    );
+  }
+}
 
-export default App;
+export default Addsign;
