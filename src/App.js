722f0d99fc17e5e7618daf660dbf60c7658b0cf3
Maciej Rybak <maciek.rybak95@gmail.com>
2022-11-13 15:31:07 +0100

    Counter

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 import "./App.css";
 
 const App = (props) => {
-  const [state, setState] = useState({ a: 0, b: 10 });
-  // const [info, setInfo] = useState("nie łączymy w jeden state");
-  const handler = () => setState({ ...state, a: state.a + 1 });
+  const [counter, setCounter] = useState(0);
+
+  const handler = () => setCounter(counter + 1);
 
   return (
     <div onClick={handler}>
-      <h1>{state.a}</h1>
-      <h2>{state.b}</h2>
+      <h1>{counter}</h1>
+      <h2>Licznik</h2>
       <p>Lorem ipsum</p>
     </div>
   );
